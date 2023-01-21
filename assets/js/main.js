21d12fe3afc39f8571a7b51b600785b6f0c74388
Jernej <jernej.co@gmail.com>
2023-01-21 23:20:42 +0100

    feat: refactor scroll to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  // ======= Sticky
+  // ======= Back to top
   window.onscroll = function () {
     // show or hide the back-top-top button
     const backToTop = document.querySelector(".back-to-top");
@@ -108,38 +108,12 @@
   // ===== wow js
   new WOW().init();
 
-  // ====== scroll top js
-  function scrollTo(element, to = 0, duration = 500) {
-    const start = element.scrollTop;
-    const change = to - start;
-    const increment = 20;
-    let currentTime = 0;
-
-    const animateScroll = () => {
-      currentTime += increment;
-
-      const val = Math.easeInOutQuad(currentTime, start, change, duration);
-
-      element.scrollTop = val;
-
-      if (currentTime < duration) {
-        setTimeout(animateScroll, increment);
-      }
-    };
-
-    animateScroll();
-  }
-
-  Math.easeInOutQuad = function (t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return (c / 2) * t * t + b;
-    t--;
-    return (-c / 2) * (t * (t - 2) - 1) + b;
-  };
-
-  document.querySelector(".back-to-top").onclick = () => {
-    scrollTo(document.documentElement);
-  };
+  document.querySelector(".back-to-top").addEventListener("click", (e) => {
+    document.body.scrollIntoView({
+      behavior: "smooth",
+      offsetTop: 0,
+    });
+  });
 
   // ==== for menu scroll
   const pageLink = document.querySelectorAll(".ud-menu-scroll");
