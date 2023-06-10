fb5201b27a868e6c4511558e33ef97eab56eae5f
Jernej <jernej.co@gmail.com>
2023-06-10 12:35:41 +0200

    feat: welcome screen cleanup

--- src/assets/js/welcome.js
+++ src/assets/js/welcome.js
@@ -1,4 +1,6 @@
 window.onload = () => {
+
+  // Next button action
   const elements = document.getElementsByClassName("next-button");
 
   function continueEvent(event) {
@@ -13,6 +15,22 @@ window.onload = () => {
   for (var i = 0; i < elements.length; i++)
     elements[i].addEventListener('click', continueEvent, false);
 
+  // Prev button action
+  const prevElements = document.getElementsByClassName("prev-button");
+
+  function prevEvent(event) {
+    const target = event.target;
+    const parent = findParentByCls(target, 'row', 5);
+    const prev = parent.previousElementSibling;
+
+    parent.classList.remove('active');
+    prev.classList.add('active');
+  };
+
+  for (var i = 0; i < prevElements.length; i++)
+    prevElements[i].addEventListener('click', prevEvent, false);
+
+  // Helpers
   function findParentByCls(node, cls, maxTries = 0) {
     let tries = 0;
 
