0c7191a532269cb1e172a2378202355b5b912423
Jernej <jernej.co@gmail.com>
2023-07-24 16:01:25 +0200

    fix: minor

--- feed/components/header.tsx
+++ feed/components/header.tsx
@@ -14,11 +14,11 @@ export default function Header() {
       <div className="relative w-full flex items-center justify-between flex">
         <div className='w-auto'>
           <a href="/" className="navbar-logo flex items-center w-full">
-            <Image src="/assets/images/logo-infold-black.png" alt="InFold - logo" width={45} height={45} className="mr-2" />
+            <Image src="/assets/images/logo-infold-black.png" alt="InFold - logo" width={45} height={45} className="mr-2 dark:-ml-1" />
             <span className='text-2xl'>InFold</span>
           </a>
         </div>
-        <div className='text-sm mx-auto w-auto flex justify-center hidden md:flex'>we aggregate news</div>
+        <div className='text-sm mx-auto w-auto flex justify-center hidden md:flex'>we aggregate data</div>
         <div className="w-auto items-center justify-end flex">
           <ul className="ml-auto w-auto flex">
             <li className="mr-2">
