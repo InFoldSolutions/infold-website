2c71bfcc9ec79cbed4fa89996b40afcf010e7ddd
Jernej <jernej.co@gmail.com>
2023-08-04 13:16:44 +0200

    fix: body overflow

--- feed/app/layout.tsx
+++ feed/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-300 dark:bg-black overflow-y-scroll`}>
+      <body className={`${inter.className} bg-gray-300 dark:bg-black overflow-y-scroll overflow-x-hidden`}>
         {modal}
         {children}
       </body>
