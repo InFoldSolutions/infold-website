4e7461500392b18d8f060943d82adfb2abfb9792
Jernej <jernej.co@gmail.com>
2023-08-09 22:20:41 +0200

    fix: build

--- feed/components/filters.tsx
+++ feed/components/filters.tsx
@@ -60,12 +60,11 @@ export default function Filters() {
       <div className='relative max-w-screen-2xl overflow-x-scroll no-scrollbar'>
         <div className='flex flex-nowrap'>
           {topOptions.map((option: any, index: number) => (
-            <span className='group min-w-[80px] md:min-w-[95px] mr-1 md:mr-2 cursor-pointer flex justify-center'>
-              <Link className={`${(endpoint === 'top' && bucket === option.value) ? 'underline' : ''} group-hover:underline`} href={`?sort=top&time=${option.value}`} key={index}>{option.label}</Link>
+            <span className='group min-w-[80px] md:min-w-[95px] mr-1 md:mr-2 cursor-pointer flex justify-center' key={index}>
+              <Link className={`${(endpoint === 'top' && bucket === option.value) ? 'underline' : ''} group-hover:underline`} href={`?sort=top&time=${option.value}`}>{option.label}</Link>
               {(index + 1) < topOptions.length && <span className='ml-2'>|</span>}
             </span>
-          )
-          )}
+          ))}
         </div>
       </div>
     )
