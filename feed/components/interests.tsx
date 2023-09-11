9a9b6cd6b4d3316ef6df8e8f069d07afbac02101
Jernej <jernej.co@gmail.com>
2023-09-11 18:34:21 +0200

    feat: interests selection order

--- feed/components/interests.tsx
+++ feed/components/interests.tsx
@@ -10,19 +10,6 @@ export default function Interests({ interests, saveInterests }: { interests: str
 
   return (
     <div className='flex items-center flex-col my-auto pt-6 pb-2 md:py-12'>
-      <h3 className='text-2xl md:text-3xl font-bold mb-6 flex justify-center text-center items-center'>
-        <i className='fad fa-newspaper mr-4 md:mr-6' /> Get The Full Story
-      </h3>
-      <h4 className='mb-6 text-center max-w-[80%]'>
-        {`News is broken and driven by different agendas. We're here to help you get context, delve deeper, and learn more.`}
-        <Link href={'https://infold.medium.com/a-lot-of-knowledge-is-a-dangerous-thing-6a4d2560cc83'}
-          target={'blank'}
-          className='underline cursor-pointer ml-2'
-          title='A Lot of Knowledge is a Dangerous Thing - Read on Medium'>
-          read more..
-        </Link>
-      </h4>
-      <p className='mt-0.5 w-[50%] border-b-2 border-gray-200 border-dashed dark:border-gray-800 dark:border-opacity-80'></p>
       <h3 className='text-2xl font-bold mt-6 mb-2 flex justify-center text-center'>
         Tell us what you like to read about
       </h3>
@@ -43,12 +30,27 @@ export default function Interests({ interests, saveInterests }: { interests: str
           </li>
         ))}
       </ul>
-      <p className='mt-4 w-[50%] border-b-2 border-gray-200 border-dashed dark:border-gray-800 dark:border-opacity-80'></p>
+      <p className='mt-6 w-[50%] border-b-2 border-gray-200 border-dashed dark:border-gray-800 dark:border-opacity-80'></p>
       <button className={`${selected.length > 3 ? 'bg-black dark:bg-white dark:text-black' : 'bg-gray-500 dark:bg-gray-500 dark:bg-opacity:80 cursor-not-allowed'} text-white font-bold py-2 px-4 rounded mt-5 flex items-center`} onClick={() => saveInterests(selected)}>
         Continue <i className={`fad ${selected.length > 3 ? 'fa-check-circle' : 'fa-ban'} ml-2`}></i>
       </button>
       <span className={`text-sm mt-2 ${selected.length > 3 ? 'hidden' : ''} `}>Select <b>{4 - selected.length}</b> more</span>
       <span className={`cursor-pointer text-sm mt-2 hover:underline ${selected.length > 3 ? '' : 'hidden'} `}>Get personal feed</span>
+      <p className='my-6 w-[50%] border-b-2 border-gray-200 border-dashed dark:border-gray-800 dark:border-opacity-80'></p>
+      <h3 className='text-2xl md:text-3xl font-bold mb-6 flex justify-center text-center items-center'>
+        <i className='fad fa-newspaper mr-4 md:mr-6' /> Get The Full Story
+      </h3>
+      <h4 className='mb-6 text-center max-w-[80%]'>
+        {`News is broken and driven by different agendas. We're here to help you get context, delve deeper, and learn more.`}
+        <Link href={'https://infold.medium.com/a-lot-of-knowledge-is-a-dangerous-thing-6a4d2560cc83'}
+          target={'blank'}
+          className='underline cursor-pointer ml-2'
+          title='A Lot of Knowledge is a Dangerous Thing - Read on Medium'>
+          read more..
+        </Link>
+      </h4>
+      <p className='mt-0.5 w-[50%] border-b-2 border-gray-200 border-dashed dark:border-gray-800 dark:border-opacity-80'></p>
+
     </div >
   )
 }
