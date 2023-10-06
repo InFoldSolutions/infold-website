13d295f0c1745879b4dcd34d0cfb4c1cfa024ed1
Jernej <jernej.co@gmail.com>
2023-10-06 13:29:54 +0200

    fix: article layout

--- feed/components/article.tsx
+++ feed/components/article.tsx
@@ -13,9 +13,9 @@ export default function RelatedArticle({ item, last }: IRelatedArticle) {
   const firstArticle: any = item.articles[0]
 
   return (
-    <li className={`${last ? 'border-b-0' : 'border-b-2'} py-5 list-none rounded-md border-gray-200 dark:border-gray-800 dark:border-opacity-80 border-dashed last:border-b-0 last:mb-0 group`}
+    <li className={`${last ? 'border-b-0' : 'border-b-2'} py-4 list-none rounded-md border-gray-200 dark:border-gray-800 dark:border-opacity-80 border-dashed last:border-b-0 last:mb-0 group`}
       title={item.title}>
-      <div className="flex items-center mb-3">
+      <div className="flex items-center">
         <span>
           <Image src={item.source.logo} alt={item.source.name} width={80} height={80} className='w-8 h-8 max-w-none mr-2' />
         </span>
@@ -30,11 +30,11 @@ export default function RelatedArticle({ item, last }: IRelatedArticle) {
         </span>
       </div>
 
-      <ul className='flex w-auto'>
+      <ul className='flex w-auto flex-col'>
         {item.articles.map((article: any, index: number) => 
-          <li key={index}>
+          <li key={index} className='mt-3 group/article'>
             <h3 className="mb-2 text-xl font-bold flex-inline items-center">
-              {article.title} <span className={`text-blue-500 hover:underline cursor-pointer text-base opacity-50 group-hover:opacity-100`} onClick={() => window.open(firstArticle.url, '_blank')}>more..</span>
+              {article.title} <span className={`text-blue-500 underline cursor-pointer text-base opacity-10 group-hover/article:opacity-100`} onClick={() => window.open(firstArticle.url, '_blank')}>more..</span>
             </h3>
 
             <div className="text-sm truncate-2-lines">
