44b216532aa0ab6962e43efeb82e8c7791dfe32a
Jernej <jernej.co@gmail.com>
2023-11-14 10:42:22 +0100

    fix: category color

--- feed/components/story/category.tsx
+++ feed/components/story/category.tsx
@@ -15,7 +15,7 @@ export default function Category({ data }: { data: any }) {
   }, [router])
 
   return (
-    <div className='text-gray-600 dark:text-gray-300 mb-1'>
+    <div className='mb-1'>
       <span className='text-base group flex items-center cursor-pointer' onClick={openSection}>
         <i className={`fad ${data.categoryIcon} mr-3 ${data.categoryIcon !== 'fa-user-chart' ? 'text-[17px]' : ''}`} />
         <span className='group-hover:underline'>{data.category}</span>
