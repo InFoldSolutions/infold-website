147975df24eff4ea16bef9ab7aa3410d1070aa00
Jernej <jernej.co@gmail.com>
2023-11-15 17:16:09 +0100

    fix: skeleton border tweak

--- feed/components/layout/skeleton.tsx
+++ feed/components/layout/skeleton.tsx
@@ -3,7 +3,7 @@ export default function Skeleton({ number = 9 }: { number?: number }) {
   return (
     <div className="mt-2 ml-1">
       {Array(number).fill(0).map((el, index) => (
-        <div key={index} className="py-4 border-gray-200 border-b-2 border-dashed dark:border-gray-800 dark:border-opacity-80 rounded last:border-b-0 animate-pulse w-[96%]">
+        <div key={index} className="py-4 border-gray-200 border-opacity-70 border-b-2 border-dashed dark:border-gray-800 dark:border-opacity-40 rounded last:border-b-0 animate-pulse w-[96.5%]">
           <div className='flex flex-row w-full items-center gap-4 p-2 rounded'>
             <div className='rounded bg-gray-400 bg-opacity-30 dark:bg-gray-900 dark:bg-opacity-50 w-[180px] h-[108px] hidden md:flex'></div>
             <div className='flex flex-col gap-3 w-full'>
