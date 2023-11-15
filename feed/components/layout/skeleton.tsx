e5e1289e52f08adbd73d50080fdaf435f8280e88
Jernej <jernej.co@gmail.com>
2023-11-15 18:01:17 +0100

    fix: skeleton margin tweak

--- feed/components/layout/skeleton.tsx
+++ feed/components/layout/skeleton.tsx
@@ -1,7 +1,7 @@
 
 export default function Skeleton({ number = 9 }: { number?: number }) {
   return (
-    <div className="mt-2 ml-1">
+    <div className="ml-1">
       {Array(number).fill(0).map((el, index) => (
         <div key={index} className="py-4 border-gray-200 border-opacity-70 border-b-2 border-dashed dark:border-gray-800 dark:border-opacity-40 rounded last:border-b-0 animate-pulse w-[96.5%]">
           <div className='flex flex-row w-full items-center gap-4 p-2 rounded'>
