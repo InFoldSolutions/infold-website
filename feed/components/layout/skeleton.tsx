07b12f6ac8d27eb7faef8df183c4d5320a5055ed
Jernej <jernej.co@gmail.com>
2023-11-15 17:09:49 +0100

    feat: skeleton border and cleanup

--- feed/components/layout/skeleton.tsx
+++ feed/components/layout/skeleton.tsx
@@ -1,20 +1,20 @@
 
 export default function Skeleton({ number = 9 }: { number?: number }) {
   return (
-    <div className="mt-4 ml-1">
+    <div className="mt-2 ml-1">
       {Array(number).fill(0).map((el, index) => (
-        <div key={index} className="mb-2">
+        <div key={index} className="py-4 border-gray-200 border-b-2 border-dashed dark:border-gray-800 dark:border-opacity-80 rounded last:border-b-0 animate-pulse w-[96%]">
           <div className='flex flex-row w-full items-center gap-4 p-2 rounded'>
-            <div className='rounded bg-gray-400 bg-opacity-30 dark:bg-gray-900 dark:bg-opacity-50 w-[180px] h-[108px] animate-pulse hidden md:flex'></div>
+            <div className='rounded bg-gray-400 bg-opacity-30 dark:bg-gray-900 dark:bg-opacity-50 w-[180px] h-[108px] hidden md:flex'></div>
             <div className='flex flex-col gap-3 w-full'>
-              <span className='w-11/12 bg-gray-200 bg-opacity-80 dark:bg-gray-800 dark:bg-opacity-60 h-4 rounded animate-pulse'></span>
-              <span className='w-10/12 bg-gray-200 bg-opacity-80 dark:bg-gray-800 dark:bg-opacity-60 h-4 rounded animate-pulse'></span>
-              <span className='w-9/12 bg-gray-200 bg-opacity-80 dark:bg-gray-800 dark:bg-opacity-60 h-4 rounded animate-pulse'></span>
+              <span className='w-12/12 bg-gray-200 bg-opacity-80 dark:bg-gray-800 dark:bg-opacity-60 h-4 rounded'></span>
+              <span className='w-11/12 bg-gray-200 bg-opacity-80 dark:bg-gray-800 dark:bg-opacity-60 h-4 rounded'></span>
+              <span className='w-10/12 bg-gray-200 bg-opacity-80 dark:bg-gray-800 dark:bg-opacity-60 h-4 rounded'></span>
 
               <div className="flex">
-                <span className="w-2/12 bg-gray-400 bg-opacity-30 dark:bg-gray-900 dark:bg-opacity-50 h-4 rounded animate-pulse"></span>
-                <span className="w-2/12 bg-gray-400 bg-opacity-30 dark:bg-gray-900 dark:bg-opacity-50 h-4 rounded animate-pulse ml-3"></span>
-                <span className="w-2/12 bg-gray-400 bg-opacity-30 dark:bg-gray-900 dark:bg-opacity-50 h-4 rounded animate-pulse ml-3"></span>
+                <span className="w-2/12 bg-gray-400 bg-opacity-30 dark:bg-gray-900 dark:bg-opacity-50 h-4 rounded"></span>
+                <span className="w-2/12 bg-gray-400 bg-opacity-30 dark:bg-gray-900 dark:bg-opacity-50 h-4 rounded ml-3"></span>
+                <span className="w-2/12 bg-gray-400 bg-opacity-30 dark:bg-gray-900 dark:bg-opacity-50 h-4 rounded ml-3"></span>
               </div>
             </div>
           </div>
